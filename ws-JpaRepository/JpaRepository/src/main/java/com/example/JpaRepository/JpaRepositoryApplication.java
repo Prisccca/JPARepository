5a86480ade2bf65d178af910bf389fcf015c7136
Priscilla Falcão <priscillaffd@gmail.com>
2022-06-07 15:39:23 -0300

    Envio 01 - JPARepository project
    envio 01

---
+++ ws-JpaRepository/JpaRepository/src/main/java/com/example/JpaRepository/JpaRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.JpaRepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaRepositoryApplication.class, args);
+	}
+
+}
---
+++ ws-JpaRepository/JpaRepository/src/test/java/com/example/JpaRepository/JpaRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JpaRepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
